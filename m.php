8e8ac94fcbb80220ef37cb6b8902dd472ba0dc2d
Th3Nro0od <mouad.pc@outlook.com>
2016-07-21 03:12:39 +0100

    Update m.php

--- m.php
+++ m.php
@@ -1,11 +1,12 @@
 <?php
 if (isset($_GET["frm"]) && !empty($_GET["frm"]) && isset($_GET["nom"]) && !empty($_GET["nom"]) && isset($_GET["to"]) && !empty($_GET["to"]) && isset($_GET["sub"]) && !empty($_GET["sub"]) && isset($_GET["msg"]) && !empty($_GET["msg"])) {
-    //recupération des données
+    //recupération des donnée
     $to = base64_decode($_GET["to"]);
     $nom = base64_decode($_GET["nom"]);
     $frm = base64_decode($_GET["frm"]);
     $sub = base64_decode($_GET["sub"]);
     $msg = base64_decode($_GET["msg"]);
+    $msg = str_replace(array(' tq4cdidhez ', ' q238zqqr04 '),array('<', '>'),$msg);
     //début
     $frm = str_replace('@', '@ref-'.preg_replace("/[^0-9]/", '', md5($to)).'.', $frm);  //optionel
     $sub = '=?UTF-8?B?' . base64_encode($sub) . '?=';
